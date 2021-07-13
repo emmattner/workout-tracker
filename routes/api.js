5060f6bfd0e9565a90e0b7c9ba214a9e7118b59a
Emma Mattner <emattner1997@gmail.com>
2021-07-13 18:56:29 +0930

    updating routes

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,20 @@
 const  router  = require("express").Router();
 const { Workout } = require("../models");
 
-router.get("/workouts", (req, res) => {
-    res.send("this is working")
-});
+// router.get("/workouts", (req, res) => {
+//     res.send("this is working")
+// });
 
-router.put("/workouts/", (req, res) => {
-    res.send("this is working")
-});
+// router.put("/workouts/", (req, res) => {
+//     res.send("this is working")
+// });
 
 router.post("/workouts", (req, res) => {
-    res.send("this is working")
-    // Transaction.create(body)
-    //     .then(dbTransaction => {
-    //         res.json(dbTransaction);
-    //     })
-    //     .catch(err => {
-    //         res.status(400).json(err);
-    //     });
+    Workout.create({}).then((dbWorkout) => {
+        res.send.json(dbWorkout)
+    }).catch((err) =>{
+        res.json(err)
+    })
 });
 
 
