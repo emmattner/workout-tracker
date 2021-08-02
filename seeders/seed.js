bed8072da9f6a31c7b96c0d678e47daff64023ac
Emma Mattner <emattner1997@gmail.com>
2021-08-02 16:18:46 +0930

    updated path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models');
+const workout = require('../models/workouts');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
