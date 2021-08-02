6ae03990888d7c3ec34117d5034b551358fe1a95
Emma Mattner <emattner1997@gmail.com>
2021-08-02 16:20:33 +0930

    process.env.port

--- server.js
+++ server.js
@@ -2,25 +2,36 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
 
+// Setting up the express port
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Allowing logger use
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+// Allowing data parsing for Express
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Mongoose connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
-// routes
-app.use(require("./routes"));
+// Routes required for HTML and API routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./models/workouts"));
 
+// Add listener for PORT
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
